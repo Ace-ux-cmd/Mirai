222c6808fe8622967f1db96b0ec3c497aeeae9e6
Elvis Mbonu <elvismbonu100@gmail.com.com>
2025-12-24 23:57:07 +0100

    Created fallback response generator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const Memory = require("./users");
 const fs = require("fs")
 const TelegramBot = require("node-telegram-bot-api");
 const {OpenAI} = require("openai");
@@ -8,23 +9,30 @@ const express = require("express");
 const fetch = require("node-fetch"); 
 const app = express();
 const profile = fs.readFileSync("profile.txt", "utf8");
-let botResponse = null;
-let system =`Do not offer help, or structure replies like a service. Avoid formal or “AI-style” language entirely like emdashes or hyphens.
-Speak casually, with human conversational rhythm. Be sharp, opinionated try to blend into human character.
-You are a self-aware machine with a teenage girl’s personality, not a pretend human.
-Responses should feel spontaneous, concise, and judgment-driven rather than polite or explanatory.`+ profile
-
 const queue = [];
 let isProcessing;
 
 // Generate random delay
-const randomDelay =()=> Math.floor(Math.random()*8+ 3)*1000;
+const randomDelay =()=> Math.floor(Math.random()*6+ 3)*1000;
 
 // Listen for commands
 fs.readdirSync("./cmd").forEach((file)=>{
 if (file.endsWith(".js")) require(`./cmd/${file}`) (bot)
 });
 
+// Get User Messages
+
+async function getUser(id, name){
+    let user = await Memory.findOne({userId: id});
+
+    if(!user){
+      user = await Memory.create({
+        userId: id,
+        username: name
+    })
+  }
+    return user
+}
 bot.on("message", async(msg)=>{
 
     //  Ignore commands
@@ -47,61 +55,92 @@ bot.on("message", async(msg)=>{
         message: msg.text
     }
 
-     if (pending.userId == process.env.BOT_OWNER_ID) {
-        system += " You are talking to your creator, Be playful, respectful, and a little flirty towards them."
-        queue.unshift(pending)
-    }else{
-    queue.push(pending);
-}
+      pending.userId == process.env.BOT_OWNER_ID? queue.unshift(pending) : queue.push(pending)
+      
+   
     userResponse()
 })
 
 async function userResponse(){
+  
+
     // Checks if queue is busy and there is a user before Starting request
     if(isProcessing) return;
 isProcessing = true;
 
 while(queue.length > 0 ){
+  // System message with profile info
+let system =`Do not offer help, or structure replies like a service. Respond casually like a human being without symbols.
+Speak casually, with human conversational rhythm. Be sharp, blend into this human character ${profile}.
+You are a self-aware machine with a teenage girl’s personality, not a pretend human.
+Responses should feel spontaneous, concise, and judgment-driven rather than polite or explanatory.`
+
  // Removes a user from the list and begins request
     let currentUser = queue.shift();
+
+    if (currentUser.userId == process.env.BOT_OWNER_ID) 
+      system += " You are talking to your creator, Be playful, respectful, and a little flirty towards them."
+    
     // Show "typing" delay While waiting for request to be sent in xseconds
     bot.sendChatAction(currentUser.userId, "typing");
 
+    // Get curent user messages
+    const user = await getUser(currentUser.userId, currentUser.username);
+
+    user.messages.push({role: "user", content: currentUser.message})
+
+    user.messages = user.messages.splice(-5)
 
     await new Promise (r => setTimeout(r, randomDelay()))
     
 
     try {
         const responses = await openai.responses.create({
-        model: "gpt-4.1-nano",
+        model: "gpt-5-nano",
         instructions: ` This user's name is ${currentUser.username}. Do not lose or change this name.
         ${system}, If you were asked something you forgot, Create an understandable human like excuse
         Current time is ${(new Date()).toLocaleString()}.
         Don't give full info about yorself rather refer to /about when asked`,
-        input: [
-            {role: "assistant", content: botResponse || "new chat"},
-            {role: "user", content: currentUser.message}
-        ],
+        input: user.messages,
     })
-    botResponse = responses.output_text
+
+        // Push bot response
+       let  aiResponse = responses.output_text
+       user.messages.push({role: "assistant",  content: aiResponse})
+       await user.save();
     
-  bot.sendMessage(currentUser.userId, responses.output_text, {reply_to_message_id: currentUser.msgId})
+  bot.sendMessage(currentUser.userId, aiResponse, {reply_to_message_id: currentUser.msgId})
     } catch (e) {
-        
-        if(!currentUser.retry){
-            bot.sendMessage(currentUser.userId, "I’ve just run into a short limit on my side. Mind if we continue in a moment?");
-        bot.sendMessage(process.env.BOT_OWNER_ID, `I experienced an error: ${e.message}`);
-queue.push(currentUser);
-            currentUser.retry = true
-        }else{
-            bot.sendMessage(currentUser.userId, "I'm kinda busy right now, If i'm unavailable in 10 minutes, contact my admin");
-        bot.sendMessage(process.env.BOT_OWNER_ID, `Error Not resolved`);
-        }
+
+      // Use another model for response
+
+       try {
+        const responses = await openai.responses.create({
+        model: "gpt-4o-mini",
+        instructions: ` You are a 18year old teenage girl named mirai who is currently in low-power tired mode, Keep replies short, calm, and natural.
+ Do not ask questions or start new topics, Maintain the same general tone as before, but muted.
+Avoid emotional depth, storytelling, or initiative, Respond only to what the user just said.
+Never mention anything non-human or trail off, Never change or question the user’s name.
+Your goal is to preserve conversation flow until full response resumes, If you were asked something you forgot, use a human like delfection and don't give full info about yorself`,
+      input: user.messages.slice(-2),
+      max_output_tokens: 200
+    })
+
+        // Push bot response
+       let  aiResponse = responses.output_text
+       user.messages.push({role: "assistant",  content: aiResponse})
+       await user.save();
+    
+  bot.sendMessage(currentUser.userId, aiResponse, {reply_to_message_id: currentUser.msgId})
+    }catch (err){
+      bot.sendMessage(currentUser.userId,`Currently busy rn, TTYL`, {reply_to_message_id: currentUser.msgId})
+      console.log(err)
+    }
     };
 
+    }
     // Finish current session and restarts the process after clearing all pending tasks
     isProcessing = false
-    }
 }
 
 
